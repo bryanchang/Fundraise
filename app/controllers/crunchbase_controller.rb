a3ea84318ce8ab8b0c727a89123a6a993983557e
Bryan Chang <keunyoung.chang@gmail.com>
2013-05-17 09:24:31 -0700

    Crunchbase data updated.

--- app/controllers/crunchbase_controller.rb
+++ app/controllers/crunchbase_controller.rb
@@ -5,27 +5,107 @@ class CrunchbaseController < ApplicationController
   def graph
 
     Crunchbase::API.key = 'vr3yz4kad3h4ptu6k9c7zce9'
-    @steve_jobs = Crunchbase::Person.get("steve-jobs")
-    @facebook = Crunchbase::Company.get("facebook")
-    facebook_funding_rounds = @facebook.funding_rounds
-    puts facebook_funding_rounds
-    # s = Crunchbase::Search.find("wearable computing")
+    # @steve_jobs = Crunchbase::Person.get("steve-jobs")
 
-    # chart_data = {}
-    # facebook_funding_rounds.each do |f|
-    #   puts f.inspect
-    #   chart_data[f['funded_year']] = f['raised_amount']
+    # bn_startups = [
+    #   "facebook",
+    #   "linkedin",
+    #   "twitter",
+    #   "dropbox",
+    #   "pinterest",
+    #   "airbnb",
+    #   "instagrm"]
+
+    # bn_startup_funding =  bn_startups.map do |startup|
+    #   Crunchbase::Company.get(startup).funding_rounds
     # end
 
-    chart_data = facebook_funding_rounds.reduce({}) do |h, f|
-      h[f['round_code']] = f['raised_amount']/1000000
-      h
+    @facebook = Crunchbase::Company.get("facebook").funding_rounds
+    @twitter = Crunchbase::Company.get("twitter").funding_rounds
+    @pinterest = Crunchbase::Company.get("pinterest").funding_rounds
+
+    fb_data = {}
+    @facebook.each do |f|
+      puts @facebook.inspect
+      fb_data[f['round_code']] = f['raised_amount']/1000000
     end
 
+    # fb_data = @facebook.reduce do |h, f|
+    #   h[f['round_code']] = f['raised_amount']/1000000
+    #   h
+    # end
 
+    twitter_data = {}
+    @twitter.each do |t|
+      twitter_data[t['round_code']] = t['raised_amount']/1000000
+    end
 
+    p_data = {}
+    @pinterest.each do |p|
+      p_data[p['round_code']] = p['raised_amount']/1000000
+    end
 
-    render json: chart_data
+    # render json: fb_data
+
+    render json:
+
+    {
+      "graph" => {
+          "title" => "Billion Dollar Club Funding History",
+          "datasequences" => [
+            {
+              "title" => "Facebook",
+              "color" => "blue",
+              "yAxis" => {
+                "units" => {
+                  "prefix" => "$",
+                  "suffix" => "mm"
+                }
+              },
+              "datapoints" => [
+                  { "title" => fb_data, "value" => fb_data}
+                  # @facebook.each do |f|
+                  # { "title" => "#{f['round_code']}", "value" => "#{f['raised_amount']/1000000}"}
+                  # { "title" => "#{@facebook.map{|f| f['funded_year']}}", "value" => "#{@facebook.map{|f| f['raised_amount']/1000000}}"}
+              ]
+            }
+            #             {
+            #   "title" => "Twitter",
+            #   "color" => "aqua",
+            #   "yAxis" => {
+            #     "units" => {
+            #       "prefix" => "$",
+            #       "suffix" => "mm"
+            #     }
+            #   },
+            #   "datapoints" => [
+            #       { "title" => twitter_data, "value" => twitter_data}
+            #       # @facebook.each do |f|
+            #       # { "title" => "#{f['round_code']}", "value" => "#{f['raised_amount']/1000000}"}
+            #       # { "title" => "#{@facebook.map{|f| f['funded_year']}}", "value" => "#{@facebook.map{|f| f['raised_amount']/1000000}}"}
+            #   ]
+            # },
+            #             {
+            #   "title" => "Pintrest",
+            #   "color" => "red",
+            #   "yAxis" => {
+            #     "units" => {
+            #       "prefix" => "$",
+            #       "suffix" => "mm"
+            #     }
+            #   },
+            #   "datapoints" => [
+            #       { "title" => p_data, "value" => p_data}
+            #       # @facebook.each do |f|
+            #       # { "title" => "#{f['round_code']}", "value" => "#{f['raised_amount']/1000000}"}
+            #       # { "title" => "#{@facebook.map{|f| f['funded_year']}}", "value" => "#{@facebook.map{|f| f['raised_amount']/1000000}}"}
+            #   ]
+            # }
+
+          ]
+
+      }
+    }
 
   end
 
